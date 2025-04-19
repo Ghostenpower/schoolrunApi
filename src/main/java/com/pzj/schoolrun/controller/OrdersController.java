0993c33aab9dd8fe4559ba644de981c967e786c5
Ghostenpower <2315181588@qq.com>
2025-04-20 00:06:23 +0800

    手动合并Order-Management

--- src/main/java/com/pzj/schoolrun/controller/OrdersController.java
+++ src/main/java/com/pzj/schoolrun/controller/OrdersController.java
@@ -1,39 +1,76 @@
 package com.pzj.schoolrun.controller;
 
-
+import com.github.pagehelper.PageInfo;
+import com.pzj.schoolrun.entity.Orders;
+import com.pzj.schoolrun.entity.Tasks;
 import com.pzj.schoolrun.model.Result;
 import com.pzj.schoolrun.model.vo.orders.OrdersAddVO;
 import com.pzj.schoolrun.model.vo.orders.OrdersCancelVO;
 import com.pzj.schoolrun.model.vo.orders.OrdersUpdateVO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.pzj.schoolrun.service.IOrdersService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Collections;
 
 /**
  * <p>
  * 订单表 前端控制器
  * </p>
  *
- * @author 
+ * @author
  * @since 2025-04-08
  */
 @RestController
 @RequestMapping("/orders")
-public class OrdersController {
+@Slf4j
+public class OrdersController extends BaseController {
+    @Autowired
+    private IOrdersService ordersService;
+
+    @GetMapping("/getList")
+    public Result<?> list() {
+        startPage();
+        Long userId = getUserId();
+        List<Orders> list = ordersService.getAllOrders(userId);
+        return Result.success(PageInfo.of(list));
+    }
+
     @PostMapping("/add")
     public Result<?> add(@RequestBody OrdersAddVO ordersAddVO) {
-        return Result.success();
+        Orders order = Orders.builder()
+                .taskId(ordersAddVO.getTaskId())
+                .courierId(ordersAddVO.getCourierId())
+                .createdAt(LocalDateTime.now())
+                .orderStatus(1) // 假设初始状态为“待完成”
+                .build();
+
+        boolean saveResult = ordersService.save(order);
+        if (saveResult) {
+            return Result.success();
+        } else {
+            return Result.error("新增订单失败");
+        }
     }
+
 
     @PostMapping("/update")
     public Result<?> update(@RequestBody OrdersUpdateVO ordersUpdateVO) {
+        log.info("Updating order with data: {}", ordersUpdateVO);
+
+        Orders orders = Orders.builder()
+                .orderId(ordersUpdateVO.getOrderId())
+                .build();
+
+        ordersService.updateById(orders);
         return Result.success();
     }
 
     @PostMapping("/cancel")
     public Result<?> cancel(@RequestBody OrdersCancelVO ordersCancelVO) {
-        return Result.success();
+        return ordersService.cancelOrder(ordersCancelVO);
     }
 }
